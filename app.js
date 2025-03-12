89f1ac7e2b6cf4b9394d5ece9a7bf96c55e50837
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-03-12 12:44:15 +0530

    faq section

--- app.js
+++ app.js
@@ -3,3 +3,23 @@
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>0)
 })
+
+
+// FAQ
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq =>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+
+        // close icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className ==='fa-solid fa-plus'){
+            icon.className = 'fa-solid fa-minus'
+        }else{
+            icon.className = 'fa-solid fa-plus'
+        }
+
+    })
+})
