dc8b1c488e708eaf6ef7a4480ca58822463ceae2
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-03-17 20:42:09 +0530

    navbar media query and animation

--- app.js
+++ app.js
@@ -23,3 +23,28 @@ faqs.forEach(faq =>{
 
     })
 })
+
+
+// nav menu for small devices
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click',()=>{
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+
+//  close the nav menu
+
+const closeNav= ()=>{
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
+
